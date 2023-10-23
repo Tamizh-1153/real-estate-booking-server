57cc0d3e43aed24de1f19dbad9f159153b33a523
Tamizh-1153 <tamizh1153@gmail.com>
2023-10-23 09:43:59 +0530

    Update auth0Config.js

--- config/auth0Config.js
+++ config/auth0Config.js
@@ -3,7 +3,7 @@ const {auth} =require('express-oauth2-jwt-bearer')
 
 const jwtCheck = auth({
   audience: "https://real-estate-booking-tm.onrender.com",
-  issuerBaseURL: "dev-elberx4si3yrfbfm.us.auth0.com",
+  issuerBaseURL: "https://dev-elberx4si3yrfbfm.us.auth0.com/",
   tokenSigningAlg: "RS256",
 })
 
